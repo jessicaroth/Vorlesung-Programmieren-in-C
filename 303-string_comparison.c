cb5d987e4e315dcbcda1dc0ac44e984be233d782
Jessica Roth <Jessica.Roth@de.ibm.com>
2016-11-16 17:29:49 +0100

    comparison of strings

---
+++ 303-string_comparison.c
@@ -0,0 +1,29 @@
+/* Compare two strings */
+// TO DO 1: This program does not always give the correct results. What is the problem? Correct it!
+// TO DO 2: Adapt the comparison such that it can be used for providing an ordering e.g. for phone books
+
+#include <stdio.h>
+
+int main()
+{
+   char s1[100], s2[100];
+   int i, flag;
+
+   printf("Please enter string 1:\n");
+   scanf("%99[^\n]", s1); //frägt die ganze Zeile ab
+   printf("Please enter string 2:\n");
+   fflush(stdin);
+   scanf("%99[^\n]", s2);
+
+   flag=0; i=0;
+   while(s1[i] != 0)
+   {
+      if(s1[i]!=s2[i])
+         flag=1;
+      i=i+1;
+   }
+   if(flag)
+      printf("Strings are different\n");
+   else
+      printf("Strings are equal\n");
+}
