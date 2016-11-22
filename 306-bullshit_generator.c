432c10dd8a161854a7822132d140bfaadbfa6a27
Jessica Roth <Jessica.Roth@de.ibm.com>
2016-11-22 13:44:53 +0100

    generator for funny sentences

---
+++ 306-bullshit_generator.c
@@ -0,0 +1,31 @@
+// Generate 'bullshit sentences'
+// TO DO 1: In lines v=, n=, a=, ... rand%12? Why always 12? because there are 12 words per string
+// TO DO 2: Create more complex sentences e.g. by using more adjectives together or by concatenating several phrases.
+// TO DO 3: Generalize the 'You should' by allowing other random introductions, e.g. "we have to", "our company must"
+// TO DO 4: Don't write the output to the screen but copy the whole phrase into a separate string.
+// Source of words see: http://dack.com/web/bullshit.html
+
+#include <stdio.h> 
+
+int main( ) {
+	char verbs[][20]={ "benchmark", "exploit", "facilitate", "generate", "implement", "leverage", "maximize",
+		"mesh", "monetize", "optimize", "orchestrate", "redefine" };
+	char nouns[][20]={ "action-items", "applications", "bandwidth", "communities", "e-business", "experiences", 
+		"infrastructures", "methodologies", "partnerships", "synergies", "solutions", "technologies" };
+	char adjectives[][20]={ "bleeding-edge", "collaborative", "cross-platform", "distributed", "efficient",
+		"global", "holistic", "innovative", "mission-critical", "next-generation", "out-of-the-box", "user-centric" };
+	int number, v, n, a;
+
+	printf("How much bullshit do you need?");
+	scanf("%d", &number);
+    
+	while(number>0) 
+	{
+		v=rand()%12;
+		n=rand()%12;
+		a=rand()%12;
+
+		printf("Your boss says: You should %s %s %s!\n",verbs[v],adjectives[a],nouns[n]);
+		number=number-1;
+	}
+}
